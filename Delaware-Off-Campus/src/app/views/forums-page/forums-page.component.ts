b752c2bee8a5ae31ddf481a53dfe9ce1fedf4f56
Chris Outhwaite <chrisout@udel.edu>
2020-01-11 18:30:23 -0500

    first commit, added ANgularFire and Forums Module

---
+++ Delaware-Off-Campus/src/app/views/forums-page/forums-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forums-page',
+  templateUrl: './forums-page.component.html',
+  styleUrls: ['./forums-page.component.scss']
+})
+export class ForumsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public createPost(PostTitle:String) {
+	
+  }
+
+}
